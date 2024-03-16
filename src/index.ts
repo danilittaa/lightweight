fa17df1a2b385fed45e0b88ec6bbc957b8786020
Dana Todoriak <todoriak@gmail.com>
2024-03-16 14:55:04 +0200

    add routes for get methods

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import express, { Request, Response } from "express";
+import cats from "./data/cats.json";
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
+const BASE_URL = "/cats";
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log("Listening on port " + port);
+app.get(BASE_URL, (req: Request, res: Response) => {
+  res.json(cats);
+});
+
+app.get(BASE_URL + "/:id", (req: Request, res: Response) => {
+  const cat = cats.find((item) => item.id === req.params.id);
+  cat ? res.json(cat) : res.send(404);
+});
+
+app.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
 });
