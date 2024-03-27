5657f76c07fc50b989d4ffbcaa541fe4ebc73a09
Dana Todoriak <todoriak@gmail.com>
2024-03-27 18:43:20 +0200

    add create new cat method

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { router as CatRouter } from "./routers/cat-router";
 import morgan from "morgan";
+import bodyParser from "body-parser";
 
 const BASE_CAT_URL = "/cats";
 const PORT = 3000;
 
 const app = express();
 app.use(morgan("tiny"));
+app.use(bodyParser.json());
 app.use(BASE_CAT_URL, CatRouter);
 
 app.listen(PORT, () => {
--- src/routers/cat-router.ts
+++ src/routers/cat-router.ts
@@ -3,13 +3,21 @@ import cats from "../data/cats.json";
 
 const router = Router();
 
-router.get("", (req: Request, res: Response) => {
-  res.json(cats);
+const catsArray = cats;
+
+router.get("", (_, res: Response) => {
+  res.json(catsArray);
 });
 
 router.get("/:id", (req: Request, res: Response) => {
-  const cat = cats.find((item) => item.id === req.params.id);
+  const cat = catsArray.find((item) => item.id === req.params.id);
   cat ? res.json(cat) : res.send(404);
+});
+
+router.post("/", (req: Request, res: Response) => {
+  const newCat = req.body;
+  catsArray.push(newCat);
+  res.status(201).json(newCat);
 });
 
 export { router };
