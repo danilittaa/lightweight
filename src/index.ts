d7f9346c6065a13a4e2a0dc4d28fa5d9b90d25ba
Dana Todoriak <todoriak@gmail.com>
2024-04-10 20:44:11 +0300

    render cats table

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import express from "express";
+import express, { Response } from "express";
 import { router as CatRouter } from "./routers/cat-router";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import path from "path";
+import cats from "./data/cats.json";
 
 const BASE_CAT_URL = "/cats";
 const PORT = 3000;
@@ -9,7 +11,14 @@ const PORT = 3000;
 const app = express();
 app.use(morgan("tiny"));
 app.use(bodyParser.json());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use(BASE_CAT_URL, CatRouter);
+
+app.get("/", (_, res: Response) => {
+  res.render("index", { cats });
+});
 
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
