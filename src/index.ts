19531e1b6e467594506662073b0a0f1e91b4c60e
Dana Todoriak <todoriak@gmail.com>
2024-04-15 18:00:02 +0300

    add most popular breed

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,30 @@ app.set("view engine", "ejs");
 
 app.use(BASE_CAT_URL, CatRouter);
 
+const findMostPopularBreed = () => {
+  const result: Record<string, number> = {};
+  for (let i = 0; i < cats.length; i++) {
+    if (!result[cats[i].breed]) {
+      result[cats[i].breed] = 1;
+    } else if (result[cats[i].breed]) {
+      result[cats[i].breed] += 1;
+    }
+  }
+
+  let maxCatCount = 0;
+  let mostPopularBreed = "";
+
+  for (const breed in result) {
+    if (result[breed] > maxCatCount) {
+      maxCatCount = result[breed];
+      mostPopularBreed = breed;
+    }
+  }
+  return mostPopularBreed;
+};
+
 app.get("/", (_, res: Response) => {
-  res.render("index", { cats });
+  res.render("index", { cats, breed: findMostPopularBreed() });
 });
 
 app.listen(PORT, () => {
